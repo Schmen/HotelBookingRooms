c9a5138ce7bb3b9bd93fd924c1e133c1adaedcda
Dawid <Ogrodnik>
2018-04-04 11:03:37 +0200

    #2 Adding Model Room and changed name RoomTypes to RoomType

---
+++ HotelBookingRooms/HotelBookingRooms.BLL/Entities/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelBookingRooms.BLL.Entities
+{
+    class Room
+    {
+        public int Number { get; set; }
+        public int RoomNumber { get; set; }
+        public int FloorNumber { get; set; }
+        public int? RoomTypeID { get; set; }
+        public virtual RoomType RoomType { get; set; }
+    }
+}
