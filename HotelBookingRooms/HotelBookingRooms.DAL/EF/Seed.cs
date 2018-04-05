9176c186445698afee04c177245009be8bd5d68e
Schmen <gittaber@gmail.com>
2018-04-05 23:18:46 +0200

    #4 add SeedRoles.cs (User, Worker, Administrator)

--- HotelBookingRooms/HotelBookingRooms.DAL/EF/Seed.cs
+++ HotelBookingRooms/HotelBookingRooms.DAL/EF/Seed.cs
@@ -9,6 +9,34 @@ namespace HotelBookingRooms.DAL.EF
         public static void RunSeed(DbContext context, RoleManager<Role> roleManager, UserManager<User> userManager)
         {
             // Seed operations
+            SeedRoles(roleManager);
+        }
+
+        public static void SeedRoles(RoleManager<Role> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("User").Result)
+            {
+                Role role = new Role();
+                role.Name = "User";
+                IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+            }
+
+
+            if (!roleManager.RoleExistsAsync ("Worker").Result)
+            {
+                Role role = new Role();
+                role.Name = "Worker";
+                IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+                
+            }
+
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                Role role = new Role();
+                role.Name = "Administrator";
+                IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+
+            }
         }
 
     }
