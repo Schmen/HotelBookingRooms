e3be54d4eb564c5d7930e9da82089796d0e6dca5
Schmen <gittaber@gmail.com>
2018-06-15 20:13:47 +0200

    #13 Add SearchRoom.cshtml and SearchRoomVM.cs

---
+++ HotelBookingRooms/HotelBookingRooms.ViewModels/ReservationVM/SearchRoomVM.cs
@@ -0,0 +1,34 @@
+﻿using HotelBookingRooms.BLL.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HotelBookingRooms.ViewModels.ReservationVM
+{
+    public class SearchRoomVM
+    {
+        [Display(Name = "Hotel")]
+        public Hotel Hotel { get; set; }
+		//s
+        [Display(Name = "Room")]
+        [Required(ErrorMessage = "Room is required")]
+        public Room Room { get; set; }
+
+        [Display(Name = "ChkIn")]
+        [Required(ErrorMessage = "ChkIn is required")]
+        public DateTime? ChkIn { get; set; }
+
+        [Display(Name = "ChkOut")]
+        [Required(ErrorMessage = "ChkOut is required")]
+        public DateTime? ChkOut { get; set; }
+
+        [Display(Name = "NumberOfPeople")]
+        [Required(ErrorMessage = "ChkOut is required")]
+        public int NumberOfPeople { get; set; }
+
+        [Display(Name = "NumberOfBeds")]
+        [Required(ErrorMessage = "ChkOut is required")]
+        public int NumberOfBeds { get; set; }
+    }
+}
